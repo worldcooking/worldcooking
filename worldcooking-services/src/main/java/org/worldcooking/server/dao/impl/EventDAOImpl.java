0d6632b09980bdc7edc44583e420f83caef2faca
Toub <n.toublanc@gmail.com>
2011-10-31 00:34:14 +0100

    add services maven module (and parent pom)

---
+++ worldcooking-services/src/main/java/org/worldcooking/server/dao/impl/EventDAOImpl.java
@@ -0,0 +1,12 @@
+package org.worldcooking.server.dao.impl;
+
+import org.springframework.stereotype.Repository;
+import org.worldcooking.server.entity.Event;
+
+/**
+ * Plain DAO which provides only {@link org.worldcooking.server.dao.impl.GenericHibernateDAOImpl} methods
+ */
+@Repository
+public class EventDAOImpl extends GenericHibernateDAOImpl<Event, Long>  {
+    
+}
---
+++ worldcooking-services/src/test/java/org/worldcooking/server/entity/DummyEntity.java
@@ -0,0 +1,25 @@
+package org.worldcooking.server.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+@Deprecated
+public class DummyEntity {
+    
+	@Id
+	@GeneratedValue(strategy= GenerationType.SEQUENCE)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
