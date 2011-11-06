a0673dd3c3e3dca63766a8a2e6d255025faa2853
Matthieu Gaudet <gaudetmatthieu@gmail.com>
2011-11-06 12:28:08 +0100

    Administration

--- worldcooking-web-app/src/main/java/org/worldcooking/web/administration/AdministrationController.java
+++ worldcooking-web-app/src/main/java/org/worldcooking/web/administration/AdministrationController.java
@@ -3,10 +3,19 @@
  */
 package org.worldcooking.web.administration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.worldcooking.server.entity.event.Event;
+import org.worldcooking.server.entity.event.Subscription;
+import org.worldcooking.server.services.EventService;
+import org.worldcooking.web.util.ModelViewMapper;
 
 /**
  * @author MatthieuG
@@ -15,13 +24,35 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AdministrationController {
 
+	@Autowired
+	private EventService eventService;
+
 	@RequestMapping(value = "/administration", method = RequestMethod.GET)
 	public ModelAndView handleRequest() {
 		ModelAndView modelAndView = new ModelAndView("administration");
 		AdministrationResetDb resetDb = new AdministrationResetDb();
 
+		AdministrationValidate validate = new AdministrationValidate();
+
 		modelAndView.addObject("administrationResetDb", resetDb);
+		modelAndView.addObject("administrationValidate", validate);
 		return modelAndView;
 	}
 
+	@ModelAttribute("waitingSubscription")
+	public List<SubscriptionModel> populateAvailableTasks() {
+		List<SubscriptionModel> waitingSubscription = new ArrayList<SubscriptionModel>();
+
+		Event event = eventService.getFullEventById(10L);
+
+		for (Subscription s : event.getSubscriptions()) {
+			if (!s.getValidate()) {
+				SubscriptionModel destination = ModelViewMapper.getInstance()
+						.map(s, SubscriptionModel.class);
+				waitingSubscription.add(destination);
+			}
+		}
+
+		return waitingSubscription;
+	}
 }
---
+++ worldcooking-web-app/src/main/java/org/worldcooking/web/administration/AdministrationValidate.java
@@ -0,0 +1,15 @@
+package org.worldcooking.web.administration;
+
+public class AdministrationValidate {
+
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
